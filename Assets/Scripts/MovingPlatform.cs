80b77d425ea154b855d1325116b6120dce2260e6
Keenan Anderson <86087556+KeenanRAnderson@users.noreply.github.com>
2022-04-18 13:06:50 -0400

    Added Some prefabs.

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public float moveSpeed;
+
+    private void Update()
+    {
+        this.transform.position = Vector3.Lerp(startPos, endPos, Mathf.PingPong(Time.time * moveSpeed, 1.0f));
+    }  
+}
